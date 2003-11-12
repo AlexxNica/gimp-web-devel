3d8091ba058781a6cf66c59f876b64a407f9449f
Sven Neumann <neo@src.gnome.org>
2003-11-12 21:27:14 +0000

    Do a make before installing.

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-# Maybe do a make here?
+# need to set PATH here to get a recent version of xsltproc
+PATH=/home/neo/bin make
+
 rsync -rlt --delete --exclude-from=install.exclude ./ $1
