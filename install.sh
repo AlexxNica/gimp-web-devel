e889f43fb432a066a4ab3acf499c1aaf1747bd0c
Sven Neumann <neo@src.gnome.org>
2003-11-12 22:15:57 +0000

    fixed (?) the PATH

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # need to set PATH here to get a recent version of xsltproc
-PATH=/home/neo/bin make
+export PATH=/home/neo/bin:$PATH
+make
 
 rsync -rlt --delete --exclude-from=install.exclude ./ $1
