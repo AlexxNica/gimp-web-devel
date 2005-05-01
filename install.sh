011fee1778096c7a0fa1e13dcea67fcf607bc7f8
Sven Neumann <sven@gimp.org>
2005-05-01 13:15:38 +0000

    touch changelog.xml to force an update of this page.
    2005-05-01  Sven Neumann  <sven@gimp.org>
    
    	* install.sh: touch changelog.xml to force an update of this page.

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 # need to set PATH here to get a recent version of xsltproc
 export PATH=/home/neo/bin:$PATH
+
+# force rebuild of changelog.html to update RSS feed
+touch changelog.xml
 make
 
 rsync -rlt --delete --exclude-from=install.exclude ./ $1
