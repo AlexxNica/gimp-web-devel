662b32b82309ff1f65fc313618cf44c877f8af72
Sven Neumann <sven@gimp.org>
2005-07-01 19:42:50 +0000

    try a different fix for the changelog.html problem.
    2005-07-01  Sven Neumann  <sven@gimp.org>
    
            * install.sh: try a different fix for the changelog.html problem.

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@
 export PATH=/home/neo/bin:$PATH
 
 # force rebuild of changelog.html to update RSS feed
-touch changelog.xml
+rm changelog.xml
 make
 
 # seems to fix a bug somewhere else
-chmod 644 changelog.html
+# chmod 644 changelog.html
 
 rsync -rlt --delete --exclude-from=install.exclude ./ $1
