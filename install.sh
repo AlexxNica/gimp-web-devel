a07e3a24815b4c794589736a6166644517d0a613
Sven Neumann <sven@gimp.org>
2006-05-17 15:00:47 +0000

    do not set PATH, the system-wide installed version of xsltproc is now new
    2006-05-17  Sven Neumann  <sven@gimp.org>
    
    	* install.sh: do not set PATH, the system-wide installed version
    	of xsltproc is now new enough.  Force update of the front page so
    	that we always get the latest gimpdev news.

--- install.sh
+++ install.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-# need to set PATH here to get a recent version of xsltproc
-export PATH=/home/neo/bin:$PATH
-
-# force rebuild of changelog.html to update RSS feed
+# force rebuild of index.html and changelog.html to update RSS feeds
+rm index.html
 rm changelog.html
 make
-
-# seems to fix a bug somewhere else
-# chmod 644 changelog.html
 
 rsync -rlt --delete --exclude-from=install.exclude ./ $1
